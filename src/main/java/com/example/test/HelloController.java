7db369a48f1b7b95e5757a7383922ecdec2a02c9
Imran Fazilov <ifazilov3584@bths.edu>
2022-11-23 12:25:50 -0500

    period end

--- src/main/java/com/example/test/HelloController.java
+++ src/main/java/com/example/test/HelloController.java
@@ -6,9 +6,12 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label testText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        testText.setText("Is this working?");
     }
 }
